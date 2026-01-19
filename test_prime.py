6695924aa7ef7fcae9c3b10d3b8ed604c2f7b6a0
Gowri <01fe24bca252@kletech.ac.in>
2026-01-19 15:29:43 +0530

    Test for prime number

---
+++ test_prime.py
@@ -0,0 +1,10 @@
+from prime import is_prime
+
+def test_prime_number():
+    assert is_prime(7) == True
+
+def test_non_prime_number():
+    assert is_prime(10) == False
+
+def test_edge_case():
+    assert is_prime(1) == False
